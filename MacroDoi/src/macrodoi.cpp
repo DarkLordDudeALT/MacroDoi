2d33a29b9acaf619dc882b32ea70ce95784f7edd
DarkLordDudeALT <42117154+DarkLordDudeALT@users.noreply.github.com>
2020-07-09 16:15:56 -0400

    Initial Commit. Accidentally deleted my project. I'll try to recreate it.

---
+++ MacroDoi/src/macrodoi.cpp
@@ -0,0 +1,11 @@
+//============================================================================
+// Name        : MacroDoi.cpp
+// Author      : Epiphany
+// Version     : 1
+// Copyright   : I don't care lol
+// Description : Computer macros
+//============================================================================
+
+int main() {
+	return 0;
+}
